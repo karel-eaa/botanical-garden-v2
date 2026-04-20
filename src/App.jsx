1c68101e3c8b26be16f5b649d4ed24d48a179149
Misko <m.hresko.dev@gmail.com>
2026-04-20 13:56:07 +0200

    Fixed Errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useProgressStore } from "./store/progressStore";
 import LandingPage from "./views/LandingPage";
-import LandingPage2 from "./views/LandingPage2";
-import LandingPage3 from "./views/LandingPage3";
-import LandingPage4 from "./views/LandingPage4";
 import PlantDialogue from "./views/PlantDialogue";
 import Hint from "./views/Hint";
 import Map from "./views/Map";
@@ -14,9 +11,6 @@ function App() {
   const view = useProgressStore((s) => s.view);
 
   if (view === "landing") return <LandingPage />;
-  if (view === "landing2") return <LandingPage2 />;
-  if (view === "landing3") return <LandingPage3 />;
-  if (view === "landing4") return <LandingPage4 />;
   if (view === "map") return <Map />;
   if (view === "scanner") return <Scanner />;
   if (view === "roomDialogue") return <RoomDialogue />;
--- src/views/Map.jsx
+++ src/views/Map.jsx
@@ -11,18 +11,48 @@ export default function Map() {
 
   return (
     <main className="min-h-screen flex justify-center p-4 bg-[linear-gradient(0deg,_#F1F8E9_54.33%,_#C3E2FF_100%)]">
-
       {/* ✅ useMap (camelCase), self-closing */}
-      <img src="/botanical-garden-v2/images/map/overview-map.png" useMap="#image-map" alt="World map" />
+      <img
+        src="/botanical-garden-v2/images/map/overview-map.png"
+        useMap="#image-map"
+        alt="World map"
+      />
 
       {/* ✅ All <area> tags are siblings, each self-closed */}
       <map name="image-map">
-        <area target="" alt="rainforest" title="rainforest" coords="175,88,332,245" shape="rect" onClick={() => selectRoom(14)} />
-        <area target="" alt="mountain" title="mountain" coords="29,260,177,410" shape="rect" onClick={() => selectRoom(13)} />
-        <area target="" alt="dessert" title="dessert" coords="266,398,413,549" shape="rect" onClick={() => selectRoom(12)} />
-        <area target="" alt="mediterranean" title="mediterranean" coords="34,500,181,650" shape="rect"  onClick={() => selectRoom(11)} />
+        <area
+          target=""
+          alt="rainforest"
+          title="rainforest"
+          coords="175,88,332,245"
+          shape="rect"
+          onClick={() => selectRoom(14)}
+        />
+        <area
+          target=""
+          alt="mountain"
+          title="mountain"
+          coords="29,260,177,410"
+          shape="rect"
+          onClick={() => selectRoom(13)}
+        />
+        <area
+          target=""
+          alt="dessert"
+          title="dessert"
+          coords="266,398,413,549"
+          shape="rect"
+          onClick={() => selectRoom(12)}
+        />
+        <area
+          target=""
+          alt="mediterranean"
+          title="mediterranean"
+          coords="34,500,181,650"
+          shape="rect"
+          onClick={() => selectRoom(11)}
+        />
       </map>
-
     </main>
   );
 }
